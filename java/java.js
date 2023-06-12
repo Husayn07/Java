04fdd0b9bb10dd8f25306686dcd69d5ec69b3987
Husayn07 <huzain270@gmail.com>
2023-06-12 03:10:08 +0100

    all project

--- java/java.js
+++ java/java.js
@@ -1,3 +1,26 @@
-const first = document.getElementById("scr")
-addEventListener
-document.getElementById("scr").innerHTML = 7
+function sum(a, b) {
+    let sum;
+    sum = a + b;
+    return sum;
+}
+
+function sub(a, b){
+    let sum;
+    sum = a - b;
+    return sum;
+}
+function div(a, b){
+    let sum;
+    sum = a/b
+    return result;
+}
+function mut(a, b){
+    let sum;
+    sum = a*b;
+    return sum;
+}
+add = sum(10, 3)
+minus = sub(10, 3)
+
+console.log(add)
+console.log(minus)
